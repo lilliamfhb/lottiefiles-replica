ce403f8925923a7299d2842d9e6a92ef49f7724f
Lilliam <lilliam.harobernal@gmail.com>
2023-05-23 01:42:12 -0500

    avance

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -34,6 +34,177 @@
                 </div>
             </div>
         </div>
+        <div class="c-companies">
+            <div class="c-wrapper">
+                <p class="c-companies__resume mb-4 text-center">Trusted by 5.2 million+ Users from 250,000+ companies worldwide</p>
+                <ul class="c-companies__list d-flex align-items-center justify-content-center flex-wrap">
+                    <li>
+                        <div><img src="@/assets/images/companies/gojek.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/google.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/tiktok.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/airbnb.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/spotify.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/grab.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/amazon.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/nike.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/microsoft.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/instacart.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/uber.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/duolingo.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/bbc.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/cred.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/tonal.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/disney.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/swiggy.svg" alt=""></div>
+                    </li>
+                    <li>
+                        <div><img src="@/assets/images/companies/netflix.svg" alt=""></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="c-features">
+            <div class="c-wrapper">
+                <h2 class="c-features__title text-center">The new industry standard</h2>
+                <p class="c-features__resume text-center">
+                    Lottie is an open-source animation file format that’s tiny, high quality, scriptable,
+                    interactive, and can be manipulated at runtime. The top 500 apps on the App
+                    store now use Lottie to engage users and enhance conversions.
+                </p>
+                <div>
+                    <a href="">Learn more about Lottie <Icon icon="carbon:chevron-down" /></a>
+                </div>
+                <div>
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="c-comparisons">
+                                <div class="c-comparisons__item">
+                                    <figure>
+                                        <img class="w-100 h-100" src="@/assets/images/hero-ilus.png" alt="">
+                                    </figure>
+                                    <figcaption>
+                                        <div class="d-flex align-items-center justify-content-center ">
+                                            <span class="c-comparisons__item-name">Gif</span>
+                                            <span class="c-comparisons__item-feature">598 KB</span>
+                                        </div>
+                                        <div>
+                                            <span class="c-comparisons__item-extra"></span>
+                                        </div>
+                                    </figcaption>
+                                </div>
+                                <div class="c-comparisons__item">
+                                    <figure>
+                                        <img class="w-100 h-100" src="@/assets/images/hero-ilus.png" alt="">
+                                    </figure>
+                                    <figcaption>
+                                        <div class="d-flex align-items-center justify-content-center ">
+                                            <span class="c-comparisons__item-name">Lottie JSON</span>
+                                            <span class="c-comparisons__item-feature">66 kb</span>
+                                        </div>
+                                        <div>
+                                            <span class="c-comparisons__item-extra">89% smaller than GIF</span>
+                                        </div>
+                                    </figcaption>
+                                </div>
+                                <div class="c-comparisons__item">
+                                    <figure>
+                                        <img class="w-100 h-100" src="@/assets/images/hero-ilus.png" alt="">
+                                    </figure>
+                                    <figcaption>
+                                        <div class="d-flex align-items-center justify-content-center ">
+                                            <span class="c-comparisons__item-name">dotLottie</span>
+                                            <span class="c-comparisons__item-feature">8 KB</span>
+                                        </div>
+                                        <div>
+                                            <span class="c-comparisons__item-extra">99% smaller than GIF</span>
+                                        </div>
+                                    </figcaption>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="c-features__item">
+                                            <h3>
+                                                600 <span>%</span>
+                                                <p>smaller when compared to GIF</p>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="c-features__item">
+                                            <h3>
+                                                600 <span>%</span>
+                                                <p>smaller when compared to GIF</p>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="c-features__item">
+                                            <h3>
+                                                10 <span>x</span>
+                                                <p>faster to ship</p>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="c-features__item">
+                                            <h3>
+                                                10 <span>x</span>
+                                                <p>Boost in user conversion & engagement</p>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="c-features__item">
+                                            <h3>
+                                                5<span>x</span>
+                                                <p>Better Page Load Speed</p>
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
